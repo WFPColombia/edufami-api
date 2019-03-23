b24c48bcc3a547b09e36fb13b9eb9dab762ccfb7
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2019-03-24 01:24:38 +1300

    Modified models and  added versioning

--- common/models/step.js
+++ common/models/step.js
@@ -1,5 +1,27 @@
-'use strict';
+"use strict";
+
+var version = require("./version");
 
 module.exports = function(Step) {
+  Step.beforeRemote("create", function(context, step, next) {
+    context.args.data.created = Date.now();
+    context.args.data.edited = Date.now();
+    context.args.data.ownerId = 1;
+    next();
+  });
 
+  Step.beforeRemote("prototype.patchAttributes", function(context, step, next) {
+    context.args.data.edited = Date.now();
+    next();
+  });
+
+  Step.afterRemote("create", function(ctx, step, next) {
+    version.updateVersion("steps");
+    next();
+  });
+
+  Step.afterRemote("prototype.patchAttributes", function(ctx, step, next) {
+    version.updateVersion("steps");
+    next();
+  });
 };
