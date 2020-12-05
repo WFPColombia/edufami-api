8922cced445e2a45332dac79375e9041480a7005
Alejandro Tunaroza <alejandro.tunaroza@gmail.com>
2020-12-05 18:13:01 +1300

    Extract sample data to external json file

--- server/models-create-sample-data.js
+++ server/models-create-sample-data.js
@@ -2,70 +2,7 @@ console.log("models-create-sample-data...");
 var server = require("./server");
 var mysqlDs = server.dataSources.mysqlDs;
 
-const INITIAL_DATA = {
-  AppUsers: {
-    name: "AppUser",
-    data: [
-      {
-        firstName: "User",
-        lastName: "LastName",
-        email: "user@dev.com",
-        username: "user",
-        password: "qwe123",
-        terms: true,
-      },
-    ],
-  },
-  staffUsers: {
-    name: "StaffUser",
-    data: [
-      {
-        email: "admin@mail.com",
-        username: "admin",
-        password: "qwe123",
-      },
-    ],
-  },
-  trainings: {
-    name: "Training",
-    data: [
-      {
-        name: "Equfami para todos",
-        image: "image4.png",
-        app: "equfami",
-        description:
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam venenatis lobortis velit at finibus. Donec euismod maximus eleifend.",
-        created: Date.now(),
-        ownerId: 1,
-      },
-    ],
-  },
-  units: {
-    name: "Unit",
-    data: [
-      {
-        name: "Alimentación Saludable",
-        name_audio: "",
-        description:
-          "En este m&oacute;dulo identificaremos qu&eacute; es la alimentaci&oacute;n y de qu&eacute; se componen los alimentos",
-        description_audio: "string",
-        image_id: "string",
-        status: "string",
-        trainingId: 1,
-      },
-      {
-        name: "Combinaciones Saludables",
-        name_audio: "",
-        description:
-          "En este m&oacute;dulo identificaremos y haremos uso de combinaciones alimentarias saludables",
-        description_audio: "string",
-        image_id: "string",
-        status: "string",
-        trainingId: 2,
-      },
-    ],
-  },
-};
+const INITIAL_DATA = require("./sampleData.json");
 
 async function createSampleData() {
   try {
